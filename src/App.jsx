4421d7537a0da13dacf4441f85c3ac083d47476f
Matheus Germano da Costa <dev.mgermano@gmail.com>
2022-09-05 10:38:16 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import { Header } from './components/Header'
+
+import './global.css';
+
+export function App() {
+  return (
+    <>
+      <Header />
+      <h1>Hello world</h1>
+    </>
+  )
+}
