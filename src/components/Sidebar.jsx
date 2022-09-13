88a195da45c78d7f512cfde16a36046d08e449c5
Matheus Germano da Costa <dev.mgermano@gmail.com>
2022-09-13 09:50:44 -0300

    feat: Avatar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from 'phosphor-react';
+import { Avatar } from './Avatar';
 
 import styles from './Sidebar.module.css';
 
@@ -11,8 +12,7 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
-        <img
-          className={styles.avatar}
+        <Avatar
           src="https://github.com/matheus-germano.png"
         />
 
